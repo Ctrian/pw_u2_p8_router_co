3bb31dfab2ded004370e0058dd6a669aeb197ae3
Ctrian <andrescalvache47@gmail.com>
2025-06-04 19:41:37 -0500

    Taller 23

--- src/components/PokemonOption.vue
+++ src/components/PokemonOption.vue
@@ -1,40 +1,42 @@
 <template>
   <div class="options-container">
     <ul>
-        <li>P1</li>
-        <li>P2</li>
-        <li>P3</li>
-        <li>P4</li>
+      <li v-for="pokemon in pokemons" :key="pokemon.id">{{ pokemon.nombre }}</li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+    props: {
+        pokemons: {
+            type: Array,
+            required: true
+        }
+    }
+};
 </script>
 
 <style scoped>
 ul {
-    list-style-type: none;
+  list-style-type: none;
 }
 
 li {
-    background: #46c37a;
-    border-radius: 5px;
-    border: 1px solid skyblue;
-    cursor: pointer;
-    margin-bottom: 10px;
-    width: 150px;
+  background: #46c37a;
+  border-radius: 5px;
+  border: 1px solid skyblue;
+  cursor: pointer;
+  margin-bottom: 10px;
+  width: 150px;
 }
 
 .options-container {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 
 li:hover {
-    background: lemonchiffon;
+  background: lemonchiffon;
 }
 </style>
--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,20 +1,36 @@
 <template>
-    <h1>Selecciona el Pokemon Correcto</h1>
-  <pokemon-image v-bind:pokemon-id="69" :mostrar-imagen="true"/>
-  <pokemon-option />
+  <h1>Selecciona el Pokemon Correcto</h1>
+  <pokemon-image v-bind:pokemon-id="69" :mostrar-imagen="true" />
+  <pokemon-option :pokemons="vectorPokemon"/>
 </template>
 
 <script>
 import PokemonImage from "@/components/PokemonImage.vue";
 import PokemonOption from "@/components/PokemonOption.vue";
 
+import { obtenerOpcionesFachada } from "@/clients/PokemonClient.js";
+
 export default {
+  data() {
+    return {
+      vectorPokemon: [],
+    };
+  },
   components: {
     PokemonImage,
-    PokemonOption
+    PokemonOption,
+  },
+  methods: {
+    async iniciarJuego() {
+      const opciones = await obtenerOpcionesFachada(4);
+      this.vectorPokemon = opciones;
+      console.log(this.vectorPokemon);
+    },
+  },
+  mounted() {
+    this.iniciarJuego();
   },
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
